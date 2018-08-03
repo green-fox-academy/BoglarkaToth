0fcc289332c86febeddb062c3468ad9acc6d7ac0
Brad Trversy <me@bradtraversy.com>
2018-08-03 11:36:56 +0200

    2.exc

--- week-01/day-5/fizz-buzz.ts
+++ week-01/day-5/fizz-buzz.ts
@@ -1,5 +1,17 @@
 'use strict'
 
+for (let n1: number = 1; n1 <= 100; n1++) {
+    
+    if (n1 % 3 == 0 && n1 % 5 == 0) {
+        console.log("FizzBuzz");
+    } else if (n1 % 5 == 0) {
+        console.log("Buzz");
+    } else if (n1 % 3 == 0) {
+        console.log("Fizz");
+    } else {
+        console.log(n1);
+    }
+}
 
 // Write a program that prints the numbers from 1 to 100.
 // But for multiples of three print “Fizz” instead of the number
