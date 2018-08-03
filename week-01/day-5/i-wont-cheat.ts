1342fdf5a3b087c9ff0250c61c75d66042a3c4e4
Brad Trversy <me@bradtraversy.com>
2018-08-03 11:18:41 +0200

    1.excercises is finished

---
+++ week-01/day-5/i-wont-cheat.ts
@@ -0,0 +1,8 @@
+'use strict'
+
+for ( let n: number = 0 ; n < 100 ; n++ ) {
+    console.log( "I won't cheat on the exam!" )
+}
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
---
+++ week-01/day-5/print-event.ts
@@ -0,0 +1,7 @@
+'use strict'
+
+for ( let n2: number = 0 ; n2 <= 500 ; n2++) {
+    console.log(n2)
+}
+
+// Create a program that prints all the even numbers between 0 and 500
