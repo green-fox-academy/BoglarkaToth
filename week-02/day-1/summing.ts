83ff624bb66e54a4061c2ab1bdf984309db85d52
Brad Trversy <me@bradtraversy.com>
2018-08-06 15:21:03 +0200

    summing

---
+++ week-02/day-1/summing.ts
@@ -0,0 +1,35 @@
+'use string'
+
+
+function sum (megadottszam: number){
+    let summarize: number = 0;
+    for (let i: number = 0; i <= megadottszam; i++) {
+    summarize = summarize+i;
+    }
+    return summarize;
+}
+console.log(sum(10));
+// akkor kell, ha a 10et/számot/változót akarom változtatni,
+// ha maga a funkció logikája a lényeg, de más adatokkal akarom használni
+
+/*
+function sum (){
+    let megadottszam: number=10;
+    let summarize: number = 0;
+    for (let i: number = 0; i <= megadottszam; i++) {
+  
+        summarize = summarize+i;
+    }
+    return summarize;
+}
+console.log(sum());
+// akkor kell, ha egy megadott számmal használt funkcióra
+//van szükségem később
+*/
+
+
+
+
+// -  Write a function called `sum` that sum all the numbers until
+//the given parameter
+// -  The function should return the result
