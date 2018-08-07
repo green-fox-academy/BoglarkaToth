5e5e7faf4f110431b52f09b7c320abc37dffa20f
Brad Trversy <me@bradtraversy.com>
2018-08-07 11:40:25 +0200

    korrigal

---
+++ week-02/day-1-2/print-arguments.ts
@@ -0,0 +1,27 @@
+'use strict'
+
+
+
+function printParams (...param: number[]) {
+
+    console.log (param);
+  
+}
+
+printParams(1, 4, 6);
+
+
+
+
+
+let num = [7, 8, 9];
+num.forEach(function (value: number) {
+  console.log(value);
+});
+
+
+
+
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
