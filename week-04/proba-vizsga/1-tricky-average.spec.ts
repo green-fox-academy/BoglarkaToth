0893ac32987e4a7a26cabdea43e125f9e3a2b805
Toth Boglarka <tothboglarka@gmail.com>
2018-08-25 18:24:19 +0200

    tricky avarage task and test

--- week-04/proba-vizsga/1-tricky-average.spec.ts
+++ week-04/proba-vizsga/1-tricky-average.spec.ts
@@ -1,19 +1,18 @@
 'use strict'
+export{}
+
 declare function require(path: string): any;
 const test = require('tape');
 
 import { test } from 'tape';
-import getTrickyAvg from './tricky-average';
-
+import getTrickyAvg from './1-tricky-average';
 
 test('your test case 1', (t) => {
-  /*
-   * create a test case
-   */
-});
+  test.equal(getTrickyAvg([3, -1, 5, 8, 6]), 3.5);
+  test.end();
+})
 
 test('your test case 2', (t) => {
-  /*
-   * create a test case
-   */
-});
+  test.equal(getTrickyAvg([2, 6, 1, 10]), 5.5);
+  test.end();
+})
