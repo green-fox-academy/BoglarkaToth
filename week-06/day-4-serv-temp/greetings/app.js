db55e3622ff03ac3e7e84c82c8e3287b751dcf17
Toth Boglarka <tothboglarka@gmail.com>
2018-09-13 15:03:20 +0200

    feladatok2

--- week-06/day-4-serv-temp/greetings/app.js
+++ week-06/day-4-serv-temp/greetings/app.js
@@ -7,22 +7,20 @@ const PORT = 3002;
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('home', {     // views-on belüli home.ejs
-    title: 'Home page',
-    header: 'Green Fox Academy',
-  });
-});
-
-app.get('/products/:id', (req, res) => {
-  res.render('home', {
-    title: 'Products page',
-    header: req.params.id,
-    // params = : 
-    withGoodBye: req.query.withGoodBye === 'true',
-    // query = ?
-  });
+  let name = 'Guest';
+  if (req.query.name == undefined) {
+    res.render('home', {
+      title: 'Name page',
+      name: name
+    });
+  } else {
+    res.render('home', {
+      title: 'Name page',
+      name: req.query.name
+    });
+  }
 });
 
 app.listen(PORT, () => {
   console.log(`The server is up and running on port ${PORT}`);
-})
+});
